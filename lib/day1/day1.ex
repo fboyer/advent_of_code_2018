da1503a0cf0368e81f4dd1ad179db3da0588f123
Frédéric Boyer <frederic.boyer@tecsys.com>
2018-12-07 17:33:29 -0500

    Standardize the use of parse_input

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,29 +1,29 @@
 defmodule AdventOfCode2018.Day1 do
   def resulting_frequency(file) do
     File.read!(file)
+    |> parse_input()
     |> do_resulting_frequency()
   end
 
-  def do_resulting_frequency(input) do
-    input
-    |> parse_input()
-    |> Enum.sum()
-  end
-
-  defp parse_input(input) do
+  def parse_input(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 
+  def do_resulting_frequency(parsed_input) do
+    parsed_input
+    |> Enum.sum()
+  end
+
   def repeating_frequency(file) do
     File.read!(file)
+    |> parse_input()
     |> do_repeating_frequency()
   end
 
-  def do_repeating_frequency(input) do
-    input
-    |> parse_input()
+  def do_repeating_frequency(parsed_input) do
+    parsed_input
     |> Stream.cycle()
     |> Enum.reduce_while({0, MapSet.new([0])}, fn x, {prev_freq, known_freq} ->
       next_freq = prev_freq + x
